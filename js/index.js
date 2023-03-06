6e8bc1225c90f4209558928d9cac4b812b20a6a1
denzel funes <denzel.jose@hotmail.com>
2023-03-06 15:10:39 +0100

    Empiezo proyecto e-comerce

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//STRING -- cadena de texto o alfanumericas
+let empresa = "CoderHouse"
+let nombreCompleto = "Denzel Funes"
+
+//NUMBER -- valores numericos
+let numeroA = 2103
+let numeroB = 1975
+let numeroC = 2101.74
+
+const curso = "Javascript - 39470"
